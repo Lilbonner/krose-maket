02e65abef1ebce031c85e1f58a2a2f8ab3727da9
Daniyar Nasirov <lilbonnie666@gmail.com>
2025-05-26 18:52:26 +0600

    updated foggy effect in hero component

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,45 +1,57 @@
 'use client'
-
+import LegendToggleIcon from '@mui/icons-material/LegendToggle';
+import SearchIcon from '@mui/icons-material/Search';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import PanoramaFishEyeIcon from '@mui/icons-material/PanoramaFishEye';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 
 export default function Hero() {
     return (
-        <section className="relative h-screen bg-[url('/hero-bg.png')] bg-cover bg-center text-white flex items-center px-6 md:px-12 rounded-3xl">
+        <section
+            className="bg-fixed relative h-screen bg-[url('/hero-bg.png')] bg-cover bg-center text-white flex items-center px-6 md:px-12 rounded-3xl">
+            <div
+                className="absolute bottom-0 left-0 w-1/2 h-1/2 bg-gradient-to-tr from-black/40 to-transparent z-0 pointer-events-none"/>
+            <div
+                className="absolute top-0 left-0 w-1/2 h-1/2 bg-gradient-to-tr from-black/10 to-transparent z-0 pointer-events-none"/>
+            <div
+                className="absolute bottom-0 right-0 w-1/2 h-1/2 bg-gradient-to-tr from-black/10 to-transparent z-0 pointer-events-none"/>
+            <div
+                className="absolute top-0 right-0 w-1/2 h-1/2 bg-gradient-to-tr from-black/6 to-transparent z-0 pointer-events-none"/>
+
             <div className="max-w-3xl" data-aos="fade-up">
                 <p className="text-sm tracking-widest text-green-400">// WELCOME TO KROSE.</p>
                 <h1 className="text-5xl md:text-7xl font-light leading-tight mt-2">
-                    Expert your life & <br />
+                    Expert your life & <br/>
                     <span className="text-green-400 font-semibold">business coaching</span>
                 </h1>
 
                 <div className="mt-6 flex gap-4">
-                    <button className="bg-green-500 text-white px-6 py-3 rounded-full font-semibold hover:bg-green-600 transition duration-300">
+                    <button
+                        className="cursor-pointer bg-green-500 text-white px-6 py-3 rounded-full font-semibold hover:bg-green-600 transition duration-300">
                         Get In Touch →
                     </button>
-                    <button className="bg-white text-gray-800 px-6 py-3 rounded-full font-semibold hover:opacity-90 transition duration-300">
+                    <button
+                        className="cursor-pointer bg-gray-200 text-gray-800 px-6 py-3 rounded-full font-semibold hover:bg-gray-1000 transition duration-300">
                         Explore More →
                     </button>
                 </div>
 
-                <div className="mt-12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 text-sm text-white/90" data-aos="fade-up" data-aos-delay="200">
-                    <div>
-                        <p className="font-semibold">📌 Tailored Coaching Solutions</p>
-                        <p className="text-xs text-white/70">for growth.</p>
+                <div className="mt-12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5  text-gray-500 pt-32"
+                     data-aos="fade-up" data-aos-delay="200">
+                    <div className='border-t-2 border-gray-300 flex pl-12 hover:border-green-400 delay-100'>
+                        <p className="font-semibold"><LegendToggleIcon/> Tailored Coaching Solutions</p>
                     </div>
-                    <div>
-                        <p className="font-semibold">✅ Proven Coaching Techniques</p>
-                        <p className="text-xs text-white/70">Backed by experts.</p>
+                    <div className='border-t-2 border-gray-300 flex pl-12 hover:border-green-400 delay-100'>
+                        <p className="font-semibold"><SearchIcon/> Proven Coaching Techniques</p>
                     </div>
-                    <div>
-                        <p className="font-semibold">🧠 Holistic Life Coaching</p>
-                        <p className="text-xs text-white/70">and business.</p>
+                    <div className='border-t-2 border-gray-300 flex pl-12 hover:border-green-400 delay-100'>
+                        <p className="font-semibold"><ReorderIcon/> Holistic Life Coaching</p>
                     </div>
-                    <div>
-                        <p className="font-semibold">📂 Flexible Client-Centered</p>
-                        <p className="text-xs text-white/70">Programs.</p>
+                    <div className='border-t-2 border-gray-300 flex pl-12 hover:border-green-400 delay-100'>
+                        <p className="font-semibold"><PanoramaFishEyeIcon/> Flexible Client-Centered</p>
                     </div>
-                    <div>
-                        <p className="font-semibold">🏅 Expert Guidance</p>
-                        <p className="text-xs text-white/70">with Ongoing Support.</p>
+                    <div className='border-t-2 border-gray-300 flex pl-12 hover:border-green-400 delay-100'>
+                        <p className="font-semibold"><WorkspacePremiumIcon/> Expert Guidance</p>
                     </div>
                 </div>
             </div>
