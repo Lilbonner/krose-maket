669232cdaabfc63cc5696ae02ec1d64fb7a95197
Daniyar Nasirov <lilbonnie666@gmail.com>
2025-05-27 16:40:37 +0600

    added floating block in about component

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import Image from 'next/image'
+import GradeOutlinedIcon from '@mui/icons-material/GradeOutlined';
+import DoneOutlinedIcon from '@mui/icons-material/DoneOutlined';
 
 export default function About() {
     return (
@@ -9,12 +11,25 @@ export default function About() {
             id="about"
             data-aos="fade-up"
         >
-            <div className="absolute left-0 bottom-0 w-64 h-64 bg-green-100 rounded-full blur-3xl opacity-30 -z-10" />
+            <div className="absolute left-0 bottom-0 w-64 h-32 bg-green-100 rounded-full blur-3xl opacity-30 -z-10" />
+
+            <style jsx>{`
+                @keyframes floatX {
+                    0%, 100% {
+                        transform: translateX(0);
+                    }
+                    50% {
+                        transform: translateX(20px);
+                    }
+                }
+                .animate-floatX {
+                    animation: floatX 1.7s ease-in-out infinite;
+                }
+            `}</style>
 
             <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-16 items-center">
                 <div className="relative group" data-aos="zoom-in-right">
-                    <div className="absolute -top-6 -left-6 w-full h-full border-4 border-green-400 rounded-lg z-0 transition-transform duration-500 group-hover:-translate-x-2 group-hover:-translate-y-2" />
-                    <div className="relative z-10 overflow-hidden rounded-lg shadow-2xl">
+                    <div className="relative z-10 overflow-hidden rounded-lg shadow-sm fill-white ">
                         <Image
                             src="/about.png"
                             alt="About Image"
@@ -23,27 +38,31 @@ export default function About() {
                             className="object-cover rounded-lg"
                         />
                     </div>
+                    <div
+                        className={`p-4text-white flex flex-col absolute z-20 bg-[linear-gradient(263deg,#6EE7B7_0.16%,#059669_99.84%)] rounded-lg top-[150px] left-[-50px] w-[180px] min-h-[150px] animate-floatX pl-8 pt-7`}>
+                        <GradeOutlinedIcon/>
+                        <p className="font-bold text-xl text-white">30+ years of experience</p>
+                    </div>
                 </div>
 
                 <div data-aos="zoom-in-left">
                     <p className="text-green-500 text-sm tracking-widest uppercase mb-2">// About Us</p>
                     <h2 className="text-4xl md:text-5xl font-bold mb-4 leading-snug">
-                        We help you grow <br />
+                        We help you grow <br/>
                         your business
                     </h2>
-                    <p className="text-gray-600 mb-6 leading-relaxed">
-                        We are passionate about empowering individuals and organizations through strategic life and business coaching.
-                        Our tailored programs foster growth, resilience, and performance in every aspect of life.
+                    <p className="text-gray-500 mb-6 text-sm leading-relaxed">
+                        Receive tailored coaching that empowers you to navigate challenges and achieve your personal and
+                        professional goals. With expert guidance.
                     </p>
 
                     <ul className="space-y-3 mb-6 text-sm text-gray-700">
-                        <li className="flex items-center gap-2">✅ Certified professional coaches</li>
-                        <li className="flex items-center gap-2">✅ Customized client solutions</li>
-                        <li className="flex items-center gap-2">✅ Proven success record</li>
-                        <li className="flex items-center gap-2">✅ Transparent support & results</li>
+                        <li className="flex items-center gap-2"><DoneOutlinedIcon/> Certified professional coaches</li>
+                        <li className="flex items-center gap-2"><DoneOutlinedIcon/> Customized client solutions</li>
                     </ul>
 
-                    <button className="bg-green-500 text-white px-6 py-3 rounded-full font-semibold hover:bg-green-600 transition duration-300">
+                    <button
+                        className="bg-gradient-to-l from-green-600 to-green-800 text-white px-5 py-2 rounded-full font-medium hover:opacity-70 transition cursor-pointer">
                         Learn More →
                     </button>
                 </div>
