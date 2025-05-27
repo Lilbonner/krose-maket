7c48389874fe1f8660255b391cdd2bc7de2089c5
Daniyar Nasirov <lilbonnie666@gmail.com>
2025-05-27 17:18:12 +0600

    created services component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/Hero";
 import About from "@/app/components/About";
+import Services from "@/app/components/Services";
 
 export default function Home() {
     return (
         <div className="p-8">
             <Hero />
             <About />
+            <Services />
         </div>
     )
 }
