f87bede2e4a22042443769cc454e53760409f878
Daniyar Nasirov <lilbonnie666@gmail.com>
2025-05-29 16:56:53 +0600

    added info & button in news component

--- src/app/components/news.tsx
+++ src/app/components/news.tsx
@@ -1,5 +1,71 @@
-export default function News () {
+'use client'
+
+import { useEffect } from "react"
+import AOS from "aos"
+import "aos/dist/aos.css"
+import Image from "next/image";
+
+export default function News() {
+    useEffect(() => {
+        AOS.init({ duration: 800, once: true })
+    }, [])
+
+    const news = [
+        {
+            title: "Basic Package",
+            news: "Ideal for individuals starting out.",
+            highlight: false,
+            photo: "/news-post.png"
+        },
+        {
+            title: "Standard Package",
+            news: "Perfect for small teams with growing goals.",
+            highlight: true,
+            photo: "/news-post.png"
+        },
+        {
+            title: "Premium Package",
+            news: "Advanced tools for enterprise success.",
+            highlight: false,
+            photo: "/news-post.png"
+        }
+    ]
+
     return (
-        <div>news</div>
+        <section className="relative bg-[#F7F7F5] py-24 px-6 lg:px-20 rounded-3xl">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-16 items-start relative z-10">
+                <div className="sticky top-32 self-start h-fit" data-aos="fade-up" data-aos-delay="600">
+                    <p className="text-green-600 font-semibold text-lg">//
+                        <span className="text-green-800"> News </span>
+                    </p>
+                    <h2 className="text-4xl md:text-5xl font-semibold leading-tight mt-2">
+                        Latest updates <br /> and coaching <br />
+                        <span className="text-green-400 font-semibold">industry insights</span>
+                    </h2>
+                    <button
+                        className="bg-gradient-to-r from-green-400 to-green-600 text-white px-5 mt-10 py-2 rounded-full font-medium hover:opacity-70 transition cursor-pointer"
+                    >
+                        Contact Us →
+                    </button>
+                </div>
+
+            </div>
+
+            <div
+                className="flex flex-wrap justify-center gap-8 text-sm text-gray-700 mt-16"
+                data-aos="fade-up"
+                data-aos-delay="400"
+            >
+                <div className="flex items-center gap-2">
+                    <span className="text-green-500">✓</span> Get 30 day free trial
+                </div>
+                <div className="flex items-center gap-2">
+                    <span className="text-green-500">✓</span> No any hidden fees pay
+                </div>
+                <div className="flex items-center gap-2">
+                    <span className="text-green-500">✓</span> You can cancel anytime
+                </div>
+            </div>
+        </section>
     )
 }
