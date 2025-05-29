f50701605ea70156a435098cb6a6aa68aa5e6b56
Daniyar Nasirov <lilbonnie666@gmail.com>
2025-05-29 10:36:51 +0600

    updated icons & added responsive in howWeWork component

--- src/app/components/HowWeWork.tsx
+++ src/app/components/HowWeWork.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Briefcase, Lightbulb, Target, Users } from 'lucide-react';
+import AccessibilityNewOutlinedIcon from "@mui/icons-material/AccessibilityNewOutlined";
 
 const steps = [
     {
@@ -27,12 +28,12 @@ const steps = [
 
 export default function HowWeWork() {
     return (
-        <section className="py-20 bg-white text-center" id="how-we-work">
+        <section className="py-30 bg-white text-center" id="how-we-work">
             <p className="text-sm tracking-widest text-green-400" data-aos="fade-up" data-aos-delay="100">
                 // <span className="text-green-900 text-md font-medium">How we work</span>
             </p>
             <div className="max-w-6xl mx-auto px-4">
-                <h2 className="text-4xl font-md mb-4">Step by step process for<br/>
+                <h2 className="text-5xl font-md mb-4">Step by step process for<br/>
                     <span className="text-green-600 from-green-400 to-blue-300">achieving success</span></h2>
                 <p className="text-gray-500 mb-12">
                     Our process is designed to ensure your success every step of the way.
@@ -40,10 +41,12 @@ export default function HowWeWork() {
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
                     {steps.map((step, index) => (
-                        <div key={index} className="p-6 rounded-lg hover:shadow-lg transition">
-                            <div className="flex justify-center mb-4">
-                                {step.icon}
+                        <div key={index} className="p-6 rounded-lg hover:shadow-lg transition ">
+                            <div
+                                className="w-14 h-14 mb-4 rounded-full bg-gradient-to-br from-green-400 to-green-600 flex items-center justify-center text-white mx-auto">
+                                <AccessibilityNewOutlinedIcon/>
                             </div>
+
                             <h3 className="text-xl font-semibold mb-2">{step.title}</h3>
                             <p className="text-gray-500">{step.description}</p>
                         </div>
