5e024878745fc036a483d70aec1a7d6a703c9bb6
Daniyar Nasirov <lilbonnie666@gmail.com>
2025-05-29 11:35:57 +0600

    create marquee component

---
+++ src/app/components/Marquee.tsx
@@ -0,0 +1,7 @@
+export default function Marquee() {
+    return (
+        <div className="overflow-hidden whitespace-nowrap bg-white text-green-600 py-3">
+            gg
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Pricing from "@/app/components/Pricing";
 import OurExpertise from "@/app/components/OurExpertise";
 import VideoComp from "@/app/components/Video-comp";
 import HowWeWork from "@/app/components/HowWeWork";
+import Marquee from "@/app/components/Marquee";
 
 export default function Home() {
     return (
@@ -18,6 +19,7 @@ export default function Home() {
             <OurExpertise />
             <VideoComp />
             <HowWeWork />
+            <Marquee />
         </div>
     )
 }
